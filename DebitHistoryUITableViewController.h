b746e1a68bdec51ca3a7ead223efdd481024949e
Carlos Moraes <carlos-em@hotmail.com>
2013-11-16 15:06:43 -0200

    Adicionado histórico de débitos.

---
+++ DebitHistoryUITableViewController.h
@@ -0,0 +1,13 @@
+//
+//  DebitHistoryUITableViewController.h
+//  Piggy Bank
+//
+//  Created by OZZE on 16/11/13.
+//  Copyright (c) 2013 The Mob Project. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DebitHistoryUITableViewController : UITableViewController
+
+@end
